bed0afe5ab7760cd68f175d033c920192be7d748
scottIJ <31440830+scottIJ@users.noreply.github.com>
2018-03-01 07:59:36 -0600

    working on da lab

--- TextLab08st.java
+++ TextLab08st.java
@@ -35,13 +35,11 @@ class FullName
 
 	public String lastNameFirst()
 	{
-		/*
-		 *  80 point precondition: The entered name consists of a First Name and a Last Name seprated by a single space.
-		 *
-		 * 100 point precondition: The entered name consists of a First Name, Last Name and any number of Middle Names.
-		 *                         NOTE: "any number" means it is possible not to have no Middle Name at all.
-		 *                         You can assume the user is not entering any extra spaces.
-		 */
+		String n = name;
+		int index = n.indexOf(" ");
+		String lastName = n.substring(index);
+		String firstName = n.substring(0, index);
+		return lastName + ", " + firstName;
 
 
 
